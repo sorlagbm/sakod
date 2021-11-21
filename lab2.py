40cf8bf5c3218cb8d163af3efc88a5ad548c1fad
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2021-11-22 00:02:44 +0300

    добавлен класс дерева

--- lab2.py
+++ lab2.py
@@ -1,5 +1,49 @@
 import time
 
+class Node:
+    def __init__(self, sub="", children=None):
+        self.sub = sub
+        self.ch = children or []
+
+
+class SuffixTree:
+    def __init__(self, str):
+        self.nodes = [Node()]
+        for i in range(len(str)):
+            self.addSuffix(str[i:])
+
+    def addSuffix(self, suf):
+        n = 0
+        i = 0
+        while i < len(suf):
+            b = suf[i]
+            x2 = 0
+            while True:
+                children = self.nodes[n].ch
+                if x2 == len(children):
+                    n2 = len(self.nodes)
+                    self.nodes.append(Node(suf[i:], []))
+                    self.nodes[n].ch.append(n2)
+                    return
+                n2 = children[x2]
+                if self.nodes[n2].sub[0] == b:
+                    break
+                x2 = x2 + 1
+
+            sub2 = self.nodes[n2].sub
+            j = 0
+            while j < len(sub2):
+                if suf[i + j] != sub2[j]:
+                    n3 = n2
+                    n2 = len(self.nodes)
+                    self.nodes.append(Node(sub2[:j], [n3]))
+                    self.nodes[n3].sub = sub2[j:]
+                    self.nodes[n].ch[x2] = n2
+                    break
+                j = j + 1
+            i = i + j
+            n = n2 
+
 # Поиск подстроки с помощью Z-функции
 def zfunction(s : str) -> []:
     n = len(s)
@@ -22,10 +66,22 @@ def zsearch(text : str, pattern : str) -> int:
         if zf[i] == m:
             return i
 
+def tree_search(a, b):
+    pass
+
 def main() -> None:
     # Z-функция
     s = "string"
-    print(zsearch(s, "str"))
+    print(zsearch(s, "tri"))
+
+    c = SuffixTree("fsdasdf$")
+    c.visualize()
+    key = "das"
+    d = 0
+    for i in range(0, len(c.nodes)):
+        if c.nodes[i].sub == key:
+            d = i
+    print(d)
 
 if __name__ == '__main__':
     main()
