4ef252afce516bd3584c6b5e67220ba936f587af
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2022-02-15 20:27:31 +0300

    Update main.cpp

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "qstack.h"
+using namespace std;
 
 int main(){
+
+    qstack<int> s;
+    s.push(10);
+    cout << s.head() << endl;
+
+    if (!s.isEmpty())
+        s.pop();
+
+    try {
+        cout << s.head() << endl;
+    }
+    catch (EmptyListException& e) {
+        cout << "List is empty" << endl;
+    }
 
     return 0;
 
